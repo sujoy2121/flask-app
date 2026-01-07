78d98d58aed06f605335211e4ec82a8ae96aaa51
sujoy <you@example.com>
2026-01-07 20:32:56 +0530

    update data

--- ws.py
+++ ws.py
@@ -2054,6 +2054,7 @@ class MultiUserManager:
     #             c.start()
 
     def start_all(self):
+        self._load_initial_users()   # 🔥 ADD THIS
         with users_lock:
             for client_map in self.clients.values():
                 delta = client_map.get("delta")
