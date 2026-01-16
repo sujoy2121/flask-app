eeec06d1494d77ba248a7ba823d585b058bfdc89
sujoy <you@example.com>
2026-01-16 20:00:41 +0530

    update status

--- test/test2.py
+++ test/test2.py
@@ -1,11 +1,39 @@
-while True:
-    confirm = input("Confirm করবেন? (y/n): ").strip().lower()
+# while True:
+#     confirm = input("Confirm করবেন? (y/n): ").strip().lower()
 
-    if confirm in ["y", "yes"]:
-        print("✔ Confirmed")
-        break
-    elif confirm in ["n", "no"]:
-        print("❌ Cancelled")
-        break
-    else:
-        print("⚠ ভুল input, আবার দিন")
+#     if confirm in ["y", "yes"]:
+#         print("✔ Confirmed")
+#         break
+#     elif confirm in ["n", "no"]:
+#         print("❌ Cancelled")
+#         break
+#     else:
+#         print("⚠ ভুল input, আবার দিন")
+
+
+# from threading import Event
+# import base64
+from delta_rest_client import DeltaRestClient
+import time
+import hmac
+import hashlib
+import requests
+import json
+# import websocket
+from datetime import datetime, timezone, timedelta
+
+from delta_rest_client import DeltaRestClient,OrderType,TimeInForce
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from binance import get_binance_funding,dcx_to_binance_symbol,get_binance_funding_safe,get_live_binance_funding
+
+data = get_binance_funding_safe("BTCUSDT")
+print("data : ",data)
+
+
+
+
+
